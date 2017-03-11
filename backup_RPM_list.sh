32881669b1fcb53b938777a58dcfc5d07b0d9320
ubbn <ubbn@live.com>
2017-03-11 09:10:42 +0100

    Updated descriptions

--- backup_RPM_list.sh
+++ backup_RPM_list.sh
@@ -1,34 +1,32 @@
 #!/bin/bash
 
 : '
-
-Making RPM package list current at running time
-Useful for seeing which package corrupts system
+Tiny, yet personally useful, script to export a list of installed RPM packages, specially useful for detecting which newly installed package corrupts or breaks the system
 
 @author: ubbn
 @date: 2012-03-10
-
 '
+
 # Printer function
 function printLog (){
-	NOW=$(date +%Y-%m-%d%t%H:%M:%S)
-	
-	echo -e "$NOW\t$1"
+  NOW=$(date +%Y-%m-%d%t%H:%M:%S)
+  echo -e "$NOW\t$1"
 }
 
 # Main function
 function main(){
-	ODOO=$(date +%Y-%m-%d_%H%M%S)
-	OUTPUT="list_rpm_"$ODOO
-	OUTDIR=$HOME/Documents
+  CURRENT_TIME_STAMP=$(date +%Y-%m-%d_%H%M%S)
+  OUTPUT_FILE="rmp-list-"$CURRENT_TIME_STAMP
+  OUTDIR_DIR=/tmp
+  
+  printLog "Preparing rpm list output"
+  # Query all packages descendingly sorted by installed date
+  /usr/bin/rpm --query --all --last > $OUTPUT_DIR/$OUTPUT_FILE
 
-	printLog "Preparing rpm list output"
-	rpm -qa -last > $OUTDIR/$OUTPUT
-
-	printLog "Output dir: $OUTDIR"
-	printLog "Finished, saved as: $OUTPUT"
+  printLog "Output dir: $OUTPUT_DIR"
+  printLog "Finished, saved as: $OUTPUT_FILE"
 }
-
 
 main
 
+exit 0
